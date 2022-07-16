51822c4c209230af41deb0c213129df83b5cf8f5
Arian Ebrahimi <ribrea@icloud.com>
2022-07-16 22:41:57 +0430

    Fedit

--- landing/urls.py
+++ landing/urls.py
@@ -7,4 +7,7 @@ from .views import LandingView
 
 urlpatterns = [
     path("", LandingView.as_view(), name='home'),
+    path("about-us", LandingView.as_view(), name='about-us'),
+    path("products", LandingView.as_view(), name='products'),
+
 ]
